16a017fbeddac1fdf8c753717d91dcc97e84ca87
Сергей <Resler18@yandex.com>
2025-07-27 00:58:29 +0300

    vps-test3

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000', // Все запросы, начинающиеся с /api, будут перенаправлены на Express
+      '/api': 'http://31.41.155.175', // Все запросы, начинающиеся с /api, будут перенаправлены на Express
     },
   },
 })
