1e573998b2c603c4806712fb5fc598153daa01aa
Сергей <Resler18@yandex.com>
2025-07-28 19:40:51 +0300

    added database

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,37 +3,69 @@ import { useState, useEffect } from 'react'
 
 function App() {
     const [fruit, setFruit] = useState(null);
+    const [vegetables, setVegetables] = useState(null);
 
-  useEffect(()=> {
-    const getFruit = async() => {
-        try {
-            const response = await fetch('/api/fruit');
-            if(response.ok) {
-                const  data = await response.json();
-                console.log(data);
-                setFruit(data);
+    useEffect(()=> {
+        const getFruit = async() => {
+            try {
+                const response = await fetch('/api/fruit');
+                if(response.ok) {
+                    const  data = await response.json();
+                    console.log(data);
+                    setFruit(data);
+                }
+            } catch (e) {
+                console.log(e);
             }
-        } catch (e) {
-            console.log(e);
         }
-    }
-    getFruit();
-  }, [])
+        getFruit();
+    }, []);
 
-return (
-  <div className='fruit-container'>
-    {
-      fruit &&
-      fruit.map(item => {
-        return ( 
-          <ul key={item.id}> 
-            <li>{item.fruit}</li>
-          </ul>
-        );
-      })
-    }
-  </div>
-);
+    
+    useEffect(()=> {
+        const getVegetables = async() => {
+            try {
+                const response = await fetch('/api/vegetables');
+                if(response.ok) {
+                    const  data = await response.json();
+                    console.log(data);
+                    setVegetables(data);
+                }
+            } catch (e) {
+                console.log(e);
+            }
+        }
+        getVegetables();
+    }, []);
+
+    return (
+        <>
+            <div className='fruit-container'>
+                {
+                    fruit &&
+                    fruit.map(item => {
+                        return ( 
+                        <ul key={item.id}> 
+                            <li>{item.fruit}</li>
+                        </ul>
+                        );
+                    })
+                }
+            </div>
+            <div className='vegetables-container'>
+                <h2>Эти овощи из базы данных на русском языке</h2>
+                {
+                    vegetables ? vegetables.map(item => {
+                        return ( 
+                            <ul key={item.id}> 
+                                <li>{item.fruit}</li>
+                            </ul>
+                        );
+                    }) : <span className='vegetables-container_dont'>We didn`t get vegetables form DataBase MySQL</span>
+                }
+            </div>
+        </>
+    );
 }
 
 export default App
--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://31.41.155.175:5000', // Все запросы, начинающиеся с /api, будут перенаправлены на Express
+      '/api': 'http://localhost:5000', // Все запросы, начинающиеся с /api, будут перенаправлены на Express ,при deploy строку меняем
     },
   },
 })
